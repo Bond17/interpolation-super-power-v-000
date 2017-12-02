6a4bd81bf8461955699dcfa9855e9aa8d533bcf0
Robert Watson <spartan83@gmail.com>
2017-12-02 04:44:44 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,2 @@
-# Write your #display_rainbow method here
+def display_rainbow(rainbow.new)# Write your #display_rainbow method here
+  puts "R #{rainbow[0]} O #{rainbow[1]} O #{rainbow[1]} O #{rainbow[1]} O #{rainbow[1]} O #{rainbow[1]} O #{rainbow[1]}"
